889e5bcc1260227ad903def0a99d852128f7c8ea
All_right_Cool <ehrud3220@naver.com>
2023-11-30 16:47:26 +0900

    API key delete.ver

--- namecard/openCV_OCR.py
+++ namecard/openCV_OCR.py
@@ -35,7 +35,7 @@ def put_text(image, text, x, y, color=(0, 255, 0), font_size=22):
 
 def get_result(img_path):
 	# OpenAI API 키 설정
-	api_key = "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"
+	api_key = ""
 	openai.api_key = api_key
 	
 	# 이미지가 있는 폴더 경로 설정
